3cce8f940e96e064cca90b942ae691e5dac8a911
MayamsinArtem <maiamsin.am@phystech.edu>
2026-02-25 14:51:13 +0300

    Last

--- get-dac/mcp4725-sin.py
+++ get-dac/mcp4725-sin.py
@@ -1,7 +1,8 @@
+'''
 import mcp4725_driver as mcp
 import signal_generator as sg
 import time
-amplitude = 4.5
+amplitude = 3
 signal_frequency = 10
 sampling_frequency = 10000
 if __name__=="__main__":
@@ -17,4 +18,24 @@ if __name__=="__main__":
             except KeyboardInterrupt:
                 print('Stop')
     finally:
-        dac.deinit()   
+        dac.deinit()
+''' 
+import mcp4725_driver as pwmd
+import signal_generator as sg
+amplitude = 3
+signal_frequency = 10
+sampling_frequency = 2000
+if __name__=="__main__":
+    dac=pwmd.MCP4725(5.04)
+    try:
+        t=0.0
+        while True:
+            try:
+                norm_amp = sg.get_sin_wave_amplitude(signal_frequency, t)
+                dac.set_voltage(norm_amp * amplitude)
+                sg.wait_for_sampling_period(sampling_frequency)
+                t+=1.0/sampling_frequency
+            except KeyboardInterrupt:
+                print('Stop')
+    finally:
+        dac.deinit()    
