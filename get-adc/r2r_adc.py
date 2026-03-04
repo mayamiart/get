716553a16f3ab2b39ce4f393c89c581c0f63f7ca
Mayamsin Artem <maiamsin.am@phystech.edu>
2026-03-04 13:46:55 +0300

    1 from adc

---
+++ get-adc/r2r_adc.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+import time
+class R2R_ADC:
+    def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        self.compare_time = compare_time
+        
+        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+        self.comp_gpio = 21
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
+    def __del__(self):
+        try:
+            self.number_to_dac(0)
+            GPIO.cleanup()
+            if self.verbose:
+                print('Set 0')
+        except RuntimeError:
+            print('Runtime')
+    def number_to_dac(self, number):
+        a = [int(i) for i in bin(number)[2:].zfill(8)]
+        for i in range(len(a)):
+            GPIO.output(self.bits_gpio[i], a[i])
+    def sequential_counting_adc(self):
+        for value in range(255+1):
+            self.number_to_dac(value)
+            time.sleep(self.compare_time)
+            comparator_output = GPIO.input(self.comp_gpio)
+            if comparator_output==1:
+                return value
+        return 255
+    def get_sc_voltage(self):
+        digital_value = self.sequential_counting_adc()
+        step = self.dynamic_range/255.0
+        voltage = step*digital_value
+        if self.verbose:
+            print(digital_value, step, voltage)
+        return voltage
+if __name__ == "__main__":
+    adc=None
+    try:
+       adc=R2R_ADC(3.3,0.01, True)
+       while True:
+           voltage =adc.get_sc_voltage()
+           print("real", voltage)
+    except KeyboardInterrupt:
+        print("Stop")
+    finally:
+        if adc:
+            del adc
+        print('End')
